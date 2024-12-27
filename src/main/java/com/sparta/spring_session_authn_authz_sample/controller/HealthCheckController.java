d0c256427327765925bf9708014e5c7756a824b7
hochan <hochan9@gmail.com>
2024-12-27 14:31:15 +0900

    fix : push

---
+++ src/main/java/com/sparta/spring_session_authn_authz_sample/controller/HealthCheckController.java
@@ -0,0 +1,38 @@
+/*
+ * Created by Hochan Son on 2024. 12. 23.
+ * As part of
+ *
+ * Copyright (C)  () - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Backend Team <hc.son9@google.com>, 2024. 12. 23.
+ */
+
+package com.sparta.spring_session_authn_authz_sample.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * create on 2024. 12. 23. create by IntelliJ IDEA.
+ * create by IntelliJ IDEA.
+ *
+ * <p> 클래스 설명. </p>
+ * <p> {@link } and {@link }관련 클래스 </p>
+ *
+ * @author Hochan Son
+ * @version 1.0
+ * @see
+ * @since 지원하는 자바버전 (ex : 5+ 5이상)
+ */
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+  @GetMapping
+  public ResponseEntity<String> healthCheck() {
+    return ResponseEntity.ok("OK");
+  }
+}
